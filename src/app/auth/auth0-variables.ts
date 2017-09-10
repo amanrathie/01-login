64f48a2ba6f306afe488a1ba76c3fa02d5eb466d
Aman Rathie <amanrathie@gmail.com>
2017-09-10 01:03:11 -0300

    configuration

--- src/app/auth/auth0-variables.ts
+++ src/app/auth/auth0-variables.ts
@@ -7,5 +7,5 @@ interface AuthConfig {
 export const AUTH_CONFIG: AuthConfig = {
   clientID: 'gALKwsjpK_nbC3fuczFkcqz1IGDqo9Oi',
   domain: 'amanrathie.auth0.com',
-  callbackURL: 'http://localhost:4200/callback'
+  callbackURL: 'https://testingauth0.herokuapp.com/callback'
 };
