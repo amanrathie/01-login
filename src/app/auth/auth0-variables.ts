0519cc16a3288d380ef277562f3ed82b0b10561b
Aman Rathie <amanrathie@gmail.com>
2017-09-10 01:00:32 -0300

    First commit

---
+++ src/app/auth/auth0-variables.ts
@@ -0,0 +1,11 @@
+interface AuthConfig {
+  clientID: string;
+  domain: string;
+  callbackURL: string;
+}
+
+export const AUTH_CONFIG: AuthConfig = {
+  clientID: 'gALKwsjpK_nbC3fuczFkcqz1IGDqo9Oi',
+  domain: 'amanrathie.auth0.com',
+  callbackURL: 'http://localhost:4200/callback'
+};
